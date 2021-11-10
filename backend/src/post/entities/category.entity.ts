71793add97d00e0aa258ddfbccc72d2ca94d68af
Kaung Myat Han <arkarhtethan@gmail.com>
2021-11-10 15:37:43 +0630

    db index added

--- backend/src/post/entities/category.entity.ts
+++ backend/src/post/entities/category.entity.ts
@@ -1,7 +1,7 @@
 import { Type } from "class-transformer";
 import { IsString, IsNotEmpty } from "class-validator";
 import { CoreEntity } from "src/common/entities/core.entity";
-import { BeforeInsert, Column, Entity, OneToMany } from "typeorm";
+import { BeforeInsert, Column, Entity, Index, OneToMany } from "typeorm";
 import { Post } from "./post.entity";
 
 @Entity()
@@ -9,6 +9,7 @@ export class Category extends CoreEntity {
 
     @Column({ type: String, unique: true })
     @IsString()
+    @Index()
     @IsNotEmpty()
     @Type(() => String)
     name: string;
@@ -16,6 +17,7 @@ export class Category extends CoreEntity {
     @Column({ type: String, unique: true })
     @IsString()
     @IsNotEmpty()
+    @Index()
     @Type(() => String)
     slug: string;
 
