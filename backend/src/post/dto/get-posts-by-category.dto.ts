397e339e2c8b828485a0550957943804f4723348
Kaung Myat Han <arkarhtethan@gmail.com>
2021-11-17 19:59:33 +0630

    category field added in return type

--- backend/src/post/dto/get-posts-by-category.dto.ts
+++ backend/src/post/dto/get-posts-by-category.dto.ts
@@ -1,9 +1,11 @@
 import { CoreOutput } from "../../common/dtos/core.output";
 import { PaginationInput, PaginationOutput } from "../../common/dtos/pagination.output";
+import { Category } from "../entities/category.entity";
 import { Post } from "../entities/post.entity";
 
 class PostsByCategoryOutput extends PaginationOutput {
     posts: Post[];
+    category: Category;
 }
 
 export class GetAllPostsByCategoryDto extends PaginationInput { }
