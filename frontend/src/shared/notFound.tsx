6664a07fd763ead3334ed401d1051db2085347a7
Kaung Myat Han <arkarhtethan@gmail.com>
2021-11-17 20:07:42 +0630

    page not found component added

---
+++ frontend/src/shared/notFound.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+import CategoryItem from "../components/post/categoryItem";
+import { categoryList } from "../service/post.service";
+import { ICategory } from "../types/post.type";
+
+interface INotFound {
+    title: string;
+    message: string;
+}
+export default function NotFound ({ message, title }: INotFound) {
+
+    const [categories, setCategories] = useState<ICategory[]>([]);
+
+    useQuery(['categories'], categoryList, {
+        refetchOnWindowFocus: false,
+        onSuccess: (response) => {
+            setCategories(response.categories)
+        }
+    });
+
+    return <div className="my-28 w-1/3 mx-auto text-center space-y-3">
+        <p className="text-3xl font-bold">{title}</p>
+        <p className="">{message}</p>
+        <div className="flex border-2 border-gray-500 justify-center mt-5 flex-wrap p-4 space-x-4 space-y-2">
+            {categories && categories.map(category => <CategoryItem category={category} />)}
+        </div>
+    </div>
+}
