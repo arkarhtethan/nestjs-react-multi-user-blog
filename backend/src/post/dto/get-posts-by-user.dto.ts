4fc0edacde354e77c9da4d6676994478c1c9ddb5
Kaung Myat Han <arkarhtethan@gmail.com>
2021-11-17 19:59:44 +0630

    user field added in return type

--- backend/src/post/dto/get-posts-by-user.dto.ts
+++ backend/src/post/dto/get-posts-by-user.dto.ts
@@ -1,9 +1,11 @@
+import { User } from "src/user/entities/user.entity";
 import { CoreOutput } from "../../common/dtos/core.output";
 import { PaginationInput, PaginationOutput } from "../../common/dtos/pagination.output";
 import { Post } from "../entities/post.entity";
 
 class PostsByUserOutput extends PaginationOutput {
     posts: Post[];
+    user: User;
 }
 
 export class GetAllPostsByUserDto extends PaginationInput { }
