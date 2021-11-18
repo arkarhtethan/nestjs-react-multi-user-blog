103e348f3c8e150e982bb1df5b50c9341a4b5111
Kaung Myat Han <arkarhtethan@gmail.com>
2021-11-18 11:20:43 +0630

    resposive design added

--- frontend/src/components/post/postItem.tsx
+++ frontend/src/components/post/postItem.tsx
@@ -5,19 +5,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser, faCalendar } from '@fortawesome/free-solid-svg-icons'
 import { format } from 'timeago.js';
 import { Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 
 export default function PostItem ({ post }: IPostItemProps) {
+
+    const isDesktop = useMediaQuery({
+        query: '(min-width: 1024px)'
+    });
     const navigate = useNavigate();
     const navigateToDetails = () => {
         navigate(`/${post.id}`)
     }
     return (
-        <div className="shadow-lg p-3 bg-white" style={{ height: "240px" }}>
-            <div className="flex">
-                <div className="w-1/3">
+        <div className="shadow-lg bg-white" style={{ height: isDesktop ? "auto" : "auto" }}>
+            <div className="flex flex-col lg:flex-row">
+                <div className="lg:w-1/3 w-full bg-red-500" style={{ height: isDesktop ? "auto" : "200px" }}>
                     iamge
                 </div>
-                <div className="w-2/3">
+                <div className="lg:w-2/3 w-full p-3">
                     <h2 className="font-bold text-xl">
                         <Link to={`/${post.id}`}>{post.title}</Link>
                     </h2>
@@ -26,7 +31,7 @@ export default function PostItem ({ post }: IPostItemProps) {
                         <p><FontAwesomeIcon icon={faCalendar} size={'sm'} /> {format(post.createdAt)}</p>
                     </div>
                     <p className="mt-3">
-                        {post.summary.substr(0, 280)}...
+                        {post.summary.substr(0, isDesktop ? 280 : 150)}...
                     </p>
                     <SolidButton classes="px-2 mt-3 text-sm" text="Read More" onClick={navigateToDetails} />
                 </div>
