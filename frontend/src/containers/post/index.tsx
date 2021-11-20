9d4b866e155379a6530f81ae4e922771f37c94b8
Kaung Myat Han <arkarhtethan@gmail.com>
2021-11-20 20:07:53 +0630

    filename renamed

--- frontend/src/containers/post/index.tsx
+++ frontend/src/containers/post/index.tsx
@@ -1,4 +1,4 @@
 export { default as Posts } from "./posts";
-export { default as PostDetail } from "./postDetail";
-export { default as PostsByCategory } from "./postsByCategory";
-export { default as PostsByUser } from "./postsByUsername";
+export { default as PostDetail } from "./PostDetail";
+export { default as PostsByCategory } from "./PostsByCategory";
+export { default as PostsByUser } from "./PostsByUsername";
