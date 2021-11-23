121bc58138affe7b938f82e9b3e91e187dd91368
Kaung Myat Han <arkarhtethan@gmail.com>
2021-11-23 17:38:04 +0630

    GetCommentsByPostIdDto added

--- backend/src/comment/dto/get-comments-by-post.dto.ts
+++ backend/src/comment/dto/get-comments-by-post.dto.ts
@@ -1,6 +1,8 @@
 import { CoreOutput } from "src/common/dtos/core.output";
 import { Comment } from "../entities/comment.entity";
+import { PaginationInput } from "src/common/dtos/pagination.output";
 
+export class GetCommentsByPostIdDto extends PaginationInput { }
 export class GetCommentsByPostIdOutput extends CoreOutput {
     comments?: Comment[];
 }
